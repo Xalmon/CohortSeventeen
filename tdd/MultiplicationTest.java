5f87383aab7b9c058d3d488cef2ff58f05bff0d9
Xalmon <xalmonoyefule@gmail.com>
2023-07-23 06:59:43 +0100

    IntelliJ COMMIT 2

--- tdd/MultiplicationTest.java
+++ tdd/MultiplicationTest.java
@@ -1,15 +1,15 @@
-package tdd;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class MultiplicationTest {
-
-        @Test
-        public void testMultiplicationMethod() {
-            Multiplication test = new Multiplication();
-            int actualResult = test.multiply(5, 5);
-            assertEquals(25, actualResult);
-        }
-    }
+//package tdd;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class MultiplicationTest {
+//
+//        @Test
+//        public void testMultiplicationMethod() {
+//            Multiplication test = new Multiplication();
+//            int actualResult = test.multiply(5, 5);
+//            assertEquals(25, actualResult);
+//        }
+//    }
